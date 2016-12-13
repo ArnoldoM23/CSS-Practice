013ff7e73629f1963ae3ce1ed90c7b1fc6c7ea0f
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-12-13 03:36:46 -0800

    section testimonials styed pic animation

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -3,15 +3,17 @@ import SectionFeatures from './sectionFeature';
 import SectionMeals from './sectionMeals';
 import SectionSteps from './sectionSteps';
 import SectionCities from './sectionCities';
+import SectionTestimonials from './sectionTestimonials';
 
 export default class FrontPage extends Component {
 	render(){
 		return (
 			<div>
-			<SectionFeatures />
-			<SectionMeals />
-			<SectionSteps />
-			<SectionCities />
+				<SectionFeatures />
+				<SectionMeals />
+				<SectionSteps />
+				<SectionCities />
+				<SectionTestimonials />
 			</div>
 		);
 	}
